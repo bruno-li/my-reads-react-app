bdca812a6c6388605fd249c9d5985eef7b2845d0
Bruno Li <lisiuloong@gmail.com>
2018-08-21 18:58:13 -0400

    add search book route functionality and back route functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import { Link } from "react-router-dom"
 import * as BooksAPI from './BooksAPI'
+import SearchBooks from './SearchBooks'
 import './App.css'
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
+
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -16,7 +20,9 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-          <div className="list-books">
+     <Route path="/search" component={SearchBooks}
+       />
+       <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
@@ -177,13 +183,12 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to="/search" >Add a book</Link>
             </div>
           </div>
-        }
       </div>
     )
   }
 }
 
-export default BooksApp
+export default BooksApp;
