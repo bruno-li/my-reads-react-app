9ad8d731ce57afa194db45d60f1238adb6fde453
Bruno Li <lisiuloong@gmail.com>
2018-08-27 17:02:21 -0400

    update: render other two components to UI

--- src/AllBooks.js
+++ src/AllBooks.js
@@ -1,49 +1,72 @@
 import React, { Component } from "react";
 import SingleBookFetch from "./SingleBookFetch";
 
-
 class AllBooks extends Component {
   render() {
-          const { books } = this.props; //retrive the value from the app component props
+    const { books } = this.props; //retrive the value from the app component props
 
     return (
       <div className="list-books">
-
         <div className="list-books-title">
           <h1>My Reads</h1>
         </div>
         {/* list-books-title-div */}
+        <div className="list-books-content">
+          {/* BOOK CATEGORY SECTION */}
 
-             <div className="list-books-content">
-
-
-
-
-        <div className="bookshelf">
+          {/* CURRENTLY READING */}
+          <div className="bookshelf">
             <h2 className="bookshelf-title">Currently Reading</h2>
-           <div className="bookshelf-books">
-            <ol className="books-grid">
-            {
-                books.filter(book => book.shelf === "currentlyReading")
-                .map(book => (
-                    <SingleBookFetch
-                        book={book}
-                        key={book.id}
-                    />
-                ))
-            }   
-           </ol>
-      </div>
-      {/* bookshelf-books */}
-      </div>
-    /* //bookshelf div */
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                {books
+                  .filter(book => book.shelf === "currentlyReading")
+                  .map(book => (
+                    <SingleBookFetch book={book} key={book.id} />
+                  ))}
+              </ol>
+            </div>
+          </div>
+                    {/* CURRENTLY READING ENDS */}
+
+                     {/* WANT TO READ */}
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Want To Read</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                {books
+                  .filter(book => book.shelf === "wantToRead")
+                  .map(book => (
+                    <SingleBookFetch book={book} key={book.id} />
+                  ))}
+              </ol>
+            </div>
+          </div>
+                    {/* WANT TO READ ENDS */}
+
+
+                         {/* READ */}
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Read</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                {books
+                  .filter(book => book.shelf === "read")
+                  .map(book => (
+                    <SingleBookFetch book={book} key={book.id} />
+                  ))}
+              </ol>
+            </div>
+          </div>
+                    {/*  READ ENDS */}
 
 
 
-            </div> 
-       {/* list-book-content */}
+          {/* BOOK CATEGORY SECTION  END */}
+        </div>
+        {/* list-book-content */}
       </div>
-    //   list-books-div
+      //   list-books-div
     );
   }
 }
