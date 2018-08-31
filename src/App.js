5aff191481ec83d73e2acbc7772c1eb06a2e8651
Bruno Li <lisiuloong@gmail.com>
2018-08-31 11:40:15 -0400

    add logic for search functionality

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
 class BooksApp extends Component {
   state = {
     allBooks: [], //store books from BooksAPI
-    searchBooks: [] // filter out books user search
+    filterBooks: [] // filter out books user search
   };
 
   // retrives all books after the component is mount in the UI
@@ -20,34 +20,67 @@ class BooksApp extends Component {
   }
 
   // update shelf when a book changes shelf
-  updateShelf = (book,shelf) => {
-    BooksAPI
-    .update(book,shelf)
-    .then(updated => (BooksAPI.getAll().then((books) => {
-    this.setState({allBooks:books})
-    })))
-  }
+  updateShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(updated =>
+      BooksAPI.getAll().then(books => {
+        this.setState({ allBooks: books });
+      })
+    );
+  };
+
+  // create a promise request to search for a book in the database
+  searchBooks = query => {
+    if (query) {
+      BooksAPI.search(query).then((result) => {
+        this.updateSearchResult(result)
+        if (result.error !== "empty query") {
+          this.setState({ filterBooks: result }); // if search not empty, set the state of filter books
+        } else {
+          this.setState({ filterBooks: []});
+        }
+      });
+    } else {
+      this.setState({ filterBooks: []});
+    }
+  };
+
+  // update state of the book
+  updateSearchResult = (value) => {
+    for (let value of values) {
+      for (let book of this.state.allBooks) {
+        if (value.id === book.id) {
+          value.shelf = book.shelf;
+        }
+      }
+    }
+    this.setState({ filterBooks: values});
+  };
 
   render() {
     return (
       <div className="app">
-
-        <Route path="/search" component={SearchBooks} />
-
-          <Route
-            path="/"
-            render={() => (
-              <AllBooks
-              books={this.state.allBooks}
-              bookSelectOption={(book,shelf) => this.updateShelf(book,shelf)}
-              />        
-            )}
+        <Route path="/search"
+         render={() => <SearchBooks
+            filterBooks = {this.state.filterBooks}
+            searchBooks = {(query) => this.searchBooks(query)}
+            updateOption = {(book, shelf) => this.updateShelf(book,shelf)}
           />
 
-           <div className="open-search">
-                  <Link to="/search">Add a book</Link>
-            </div>
+          }/>
 
+        <Route
+          path="/"
+          render={() => (
+            <AllBooks
+              books={this.state.allBooks}
+              bookSelectOption={(book, shelf) => this.updateShelf(book, shelf)}
+            />
+          )}
+        />
+
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
       </div> /* app div */
     );
   }
